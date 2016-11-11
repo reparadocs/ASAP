67efe5265eec94cca87c4666aa73eee654940f0e
Rishab Hegde <rishabhegde@pcp079787pcs.wireless.calpoly.edu>
2016-11-11 10:27:45 -0800

    settings

--- asap/settings.py
+++ asap/settings.py
@@ -145,11 +145,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+#STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 #MEDIA FILE (user uploaded files)
-MEDIA_ROOT = os.path.join(BASE_DIR, "..", "www", "media")
-MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, "..", "www", "media")
+#MEDIA_URL = '/media/'
 
