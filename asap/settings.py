9a583ffb201e5bda54048a8b6bcb91dec46b218a
Rishab Hegde <rishabhegde@pcp079787pcs.wireless.calpoly.edu>
2016-11-11 10:55:54 -0800

    wat

--- asap/settings.py
+++ asap/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, '../static')
 #STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
